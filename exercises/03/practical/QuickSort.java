5cd0c58cf62424fc9c18e0a15ddd993c557d599d
Michael Senn <michael@senn.email>
2018-03-10 21:00:57 +0100

    Replace provided quicksort class with custom one.

--- exercises/03/practical/QuickSort.java
+++ exercises/03/practical/QuickSort.java
@@ -3,53 +3,52 @@ import java.util.Comparator;
 import java.util.Random;
 
 /**
- * File: QuickSort.java
- *
- * Implementation von Quicksort. Verwendet generische ArrayList für Eingabedaten
- * und generischen Comparator um Datenelemente zu vergleichen. 
+ * Allow sorting of generic lists using recursive quicksort.
  */
 public class QuickSort {
 	private static Random rnd = new Random();
 
-	/**
-	 * Sortiert den array zwischen den Indizes left und right (mit Grenzen).
-	 */
 	public static <T> void quickSort(ArrayList<T> array, int left, int right, Comparator<T> comp) {
-		if (right>left) {  // Abbruchbedingung der Rekursion
-			T temp;        // temporaere Hilfsvariable zum swappen
+		if (left < right) {
+			int i = partition(array, left, right, comp);
 
-			// - Assume our partition's elements go from indices m to n.
-			// - nextInt(upper) will return an integer within [0, upper[
-			// - We want an index within [m, n]
-			// Hence: [m, n] == [m, n + 1[ == m + [0, n - m +1[
-			int randomPivotIndex = left + rnd.nextInt(right - left + 1);
-
-			// *** 1. Pivotelement selektieren:
-			T pivot = array.get(randomPivotIndex);
-
-			// *** 2. Aufteilung in Subsequenzen durchfuehren:
-			int l = left-1;
-			int r = right;
-			do {
-				do l++; while (comp.compare(array.get(l),pivot)<0);
-				do r--; while (r>l && comp.compare(array.get(r),pivot)>0);
-
-				// swap(array, l, r):
-				temp = array.get(l);
-				array.set(l, array.get(r));
-				array.set(r, temp);
-			} while (r>l);
-
-				array.set(r, array.get(l));   // Korrektur: einmal zuviel getauscht
-
-				// Pivotelement in sortierte Position bringen:
-				array.set(l, pivot);
-				array.set(randomPivotIndex, temp);
-
-				// *** 3. Rekursiv die beiden Subarrays sortieren:
-				quickSort(array, left, l-1, comp);
-				quickSort(array, l+1, right, comp);
+			quickSort(array, left, i, comp);
+			quickSort(array, i + 1, right, comp);
 		}
+
+	}
+
+	private static <T> int partition(ArrayList<T> array, int left, int right, Comparator<T> comp) {
+		// - Assume our partition's elements go from indices m to n.
+		// - nextInt(upper) will return an integer within [0, upper[
+		// - We want an index within [m, n]
+		// Hence: [m, n] == [m, n + 1[ == m + [0, n - m +1[
+		int randomPivotIndex = left + rnd.nextInt(right - left + 1);
+		T pivot = array.get(randomPivotIndex);
+
+		int l = left - 1;
+		int r = right + 1;
+
+		do {
+			// Find leftmost element bigger than pivot.
+			do {
+				l++;
+			} while (comp.compare(array.get(l), pivot) < 0);
+
+			// Find rightmost element smaller than pivot.
+			do {
+				r--;
+			} while (comp.compare(array.get(r), pivot) > 0);
+
+			if (l >= r) {
+				return r;
+			}
+
+			// Swap two elements
+			T tmp = array.get(l);
+			array.set(l, array.get(r));
+			array.set(r, tmp);
+		} while (true);
 	}
 }
 
