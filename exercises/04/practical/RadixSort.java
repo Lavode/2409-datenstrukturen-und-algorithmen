789849dcbdf629b63646721c4370e7671dbc70f3
Michael Senn <michael@senn.email>
2018-03-20 22:55:53 +0100

    Improve Radix-sort.
    Order input words by length beforehand, allowing to only compare those
    which are long enough to have a char at the to-be-compared position.
    This vastly decreases time spent on what are essential no-ops.
    
    Also replace parts of the 1980-style C code with something resembling
    modern Java.

--- exercises/04/practical/RadixSort.java
+++ exercises/04/practical/RadixSort.java
@@ -1,3 +1,5 @@
+import java.util.List;
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class RadixSort {
@@ -12,44 +14,76 @@ public class RadixSort {
 	 * @param A an array of character arrays with different lengths
 	 * @param d the length of the longest array in A 
 	 */
-	public static void radixSort(char[][] A, int d)
-	{
-		// 27 queues for 26 characters plus 'empty' character
-		LinkedList[] queues = new LinkedList[27];
-		
-		// for all positions from right to left
-		for(int j=d-1; j>=0; j--)
-		{
-			// initialize empty queues
-			for(int i=0; i<27; i++) queues[i] = new LinkedList();
-			
-			// place each character array in correct queue
-			for(int i=0; i<A.length; i++)
-			{
-				if(j<A[i].length)
-				{
-					// characters 'a'-'z'
-					queues[A[i][j]-'a'+1].addLast(A[i]);
-				} 
-				else
-				{
-					// character array is shorter than current position.
-					// place it in 'empty' queue. 'emtpy' queue is queue 0
-					// to get lexicographically correct results, i.e., a<ab.
-					queues[0].addLast(A[i]);
+	public static void radixSort(char[][] A, int d) {
+
+		int[] lengths = sortByLength(A, d);
+
+		// 26 queues for 26 letters.
+		List<LinkedList<char[]>> queues = new LinkedList<LinkedList<char[]>>();
+
+		// Iterate over all to-be-compared chars, starting at
+		// right-most char.
+		for(int j = d - 1; j >= 0; j--) {
+			queues.clear();
+			for(int i = 0; i < 26; i++) {
+				queues.add(new LinkedList<char[]>());
+			}
+
+			// Iterate over words long enough to compare character
+			// at index j.
+			// Mind: If you want to compare a char at index j = 4,
+			// the word must have a length of at least 5.
+			for(int i = A.length - lengths[j + 1]; i < A.length; i++) {
+				char comparisonChar = A[i][j];
+				int listOffset = comparisonChar - 'a';
+
+				queues.get(listOffset).addLast(A[i]);
+			}
+
+			// Put newly-sorted words back into char array.
+			int n = A.length - lengths[j + 1];
+			for (LinkedList<char[]> queue : queues) {
+				for (char[] word : queue) {
+					A[n] = word;
+					n++;
 				}
 			}
-			
-			// traverse queues 
-			int n = 0;
-			for(int i=0; i<27; i++)
-			{				
-				while(queues[i].size() > 0) 
-				{
-					A[n] = (char[])queues[i].removeFirst();
-					n++;
-				} 
+		}
+	}
+
+	/**
+	 * Sorts supplied char array in-place by length ascendingly.
+	 *
+	 * Returns an array, with x[i] being the number of items in A with
+	 * length >= i.
+	 */
+	private static int[] sortByLength(char[][] A, int d) {
+		List<LinkedList<char[]>> queues = new ArrayList<LinkedList<char[]>>();
+		int[] lengths = new int[d + 1];
+		for (int i = 0; i <= d; i++) {
+			lengths[i] = 0;
+			queues.add(new LinkedList<char[]>());
+		}
+
+
+		for (char[] word : A) {
+			int wordLength = word.length;
+			lengths[wordLength]++;
+			queues.get(wordLength).addLast(word);
+		}
+
+		for (int i = lengths.length - 2; i >= 0; i--) {
+			lengths[i] = lengths[i + 1] + lengths[i];
+		}
+
+		int i = 0;
+		for (LinkedList<char[]> queue : queues) {
+			for (char[] word : queue) {
+				A[i] = word;
+				i++;
 			}
 		}
+
+		return lengths;
 	}
 }
